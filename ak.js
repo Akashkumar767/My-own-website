64f8dfb7d069cc783390bd5adcc5e68f8810f58b
Akash kumar <akkumarakash42@gmail.com>
2025-07-16 21:46:57 +0530

    describe yoor change

--- ak.js
+++ ak.js
@@ -2,3 +2,23 @@ function toggleMenu(){
     const menu = document.getElementById("menu");
     menu.style.display = menu.style.display === "block" ? "none: "block";
     }
+
+const audio = document.getElementById("audi");
+const playPause = document.getElementById("playPause");
+const progress = document.getElementById("progress");
+playPause.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    playPause.textContent = "⏸";
+  } else {
+    audio.pause();
+    playPause.textContent = "▶";
+  }
+});
+audio.addEventListener("timeupdate", () => {
+  progress.value = audio.currentTime;
+  progress.max = audio.duration;
+});
+progress.addEventListener("input", () => {
+  audio.currentTime = progress.value;
+});
